04092b9f744255ce2f7462a53f9fff7534a089ee
Mehul Potnuru <mehul.potnuru@scaler.com>
2023-04-10 17:08:03 +0530

    feat: emi price changes for May

--- dsa.rb
+++ dsa.rb
@@ -1469,8 +1469,8 @@ class AddCoursesSeoPageContent < ActiveRecord::Migration[5.1]
       content.merge!(:join => {:heading => "Connect with an Academic Counsellor", :button => [{:id => "request_callback__one", 
                   :text => "REQUEST A CALLBACK", :link => ""}]})
       content.merge!(:tuition_fee => {:heading => "<b>Tuition Fee<b>", :title_img => "academy/svg/money-logo.svg", 
-                  :subtitle => "Total fee: Rs 3.19L inclusive of GST at super affordable EMI options. Try the course for the first 2 weeks - full money-back guarantee if you choose to withdraw.", :title_content => "With EMI options, your payment can be as 
-                  low as Rs 10,492 per month - that's like your monthly grocery bill!", 
+                  :subtitle => "Total fee: Rs 3.49L inclusive of GST at super affordable EMI options. Try the course for the first 2 weeks - full money-back guarantee if you choose to withdraw.", :title_content => "With EMI options, your payment can be as 
+                  low as Rs 8,112 per month - that's like your monthly grocery bill!", 
                   :buttons => [{:id => "tuition_modal_one", :text => "VIEW EMI PLANS", :link => ""}]})
       faq_list1 = [{:heading => "<b>Why are data structures important for coding interviews?</b>", 
                   :desc => "As a software engineer/programmer, one should understand the core concepts 
--- system-design.rb
+++ system-design.rb
@@ -766,9 +766,9 @@ class AddCoursesSeoPageContent < ActiveRecord::Migration[5.1]
         :join => {:heading => "Connect with an Academic Counsellor", :button => [{:id => "request_callback__one", 
         :text => "REQUEST A CALLBACK", :link => ""}]},
         :tuition_fee => {:heading => "<b>Tuition Fee of <span class='highlight'>Scaler Academy Program<span><b>", :title_img => "academy/svg/money-logo.svg", 
-            :subtitle => "Total fee: Rs 3.19L inclusive of GST. You can get super 
+            :subtitle => "Total fee: Rs 3.49L inclusive of GST. You can get super 
             affordable EMI options. Try the course for the first 2 weeks - full money-back guarantee 
-            if you choose to withdraw.", :title_content => "With EMI options, your payment can be as low as 10,492 per month- that's like your monthly grocery bill!", 
+            if you choose to withdraw.", :title_content => "With EMI options, your payment can be as low as 8,112 per month- that's like your monthly grocery bill!", 
             :buttons => [{:id => "tuition_modal_one", :text => "VIEW EMI PLANS", :link => ""}]},
         :faq => {:main => [{:heading => "Program", :list => faq_list1}]}}
                               
